ef234f9dc3607df35b5cff11b5e7c0b56b5b7d6d
Jake Peterson <>
2021-05-13 18:50:29 +0000

    completed

--- main.rb
+++ main.rb
@@ -1,2 +1,11 @@
 # Write your code here
+number = 0
+while number < 20
+  if number.even?
+    puts "#{number} is an even number!"
+  else 
+    puts "#{number} is an odd number"
+  end
+  number += 1
+end
 
